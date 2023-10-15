d880be26e9fe7c89f08b64ad7efd3a510d3a2df4
Evald <ashotlalkin@gmail.com>
2023-10-16 01:10:54 +0200

    added navbar

--- src/main/java/com/myproject/onlineshop/homepage/HomepageController.java
+++ src/main/java/com/myproject/onlineshop/homepage/HomepageController.java
@@ -1,4 +1,13 @@
 package com.myproject.onlineshop.homepage;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomepageController {
+
+    @GetMapping("")
+    public String homePage(){
+        return "homepage";
+    }
 }
