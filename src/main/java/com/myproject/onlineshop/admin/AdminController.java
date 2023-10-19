f47c36580c6b8e1375be352cde25dfe31e9d2d3c
Evald2000 <evalddenhof@gmail.com>
2023-10-19 20:03:27 +0200

    update

--- src/main/java/com/myproject/onlineshop/admin/AdminController.java
+++ src/main/java/com/myproject/onlineshop/admin/AdminController.java
@@ -1,4 +1,15 @@
 package com.myproject.onlineshop.admin;
 
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class AdminController {
+
+    @GetMapping("AdminPage")
+    public String menu(){
+
+        return "adminMenu";
+    }
 }
